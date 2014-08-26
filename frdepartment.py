3c858f4421d61660f51f43d6df1fd19f64268ae0
Fauve <contact@taniere.info>
2014-08-26 03:30:30 +0100

    Revert "The correspondance table was added and now, the script is responding"
    This reverts commit 76a4a6118f2bb7ac76a647fe72f0dde6ad498ec4.
    
    Conflicts:
    	frdepartment.py

--- frdepartment.py
+++ frdepartment.py
@@ -2,108 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import cli.app # probably not necessary
-import sys
 import argparse
 from optparse import OptionParser
 
-coresp = [
-["Ain", "01", "AN","Rhône-Alpes","regularDepartment"],
-["Aisne", "02", "AS","Picardie","regularDepartment"],
-["Allier", "03", "AL","Auvergne","regularDepartment"],
-["Alpes-de-Haute-Provence", "04", "AH","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Hautes-Alpes", "05", "HA","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Alpes-Maritimes", "06", "AM","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Ardèche", "07", "AC","Rhône-Alpes","regularDepartment"],
-["Ardennes", "08", "AR","Champagne-Ardenne","regularDepartment"],
-["Ariège", "09", "AG","Midi-Pyrénées","regularDepartment"],
-["Aube", "10", "AB","Champagne-Ardenne","regularDepartment"],
-["Aude", "11", "AD","Languedoc-Roussillon","regularDepartment"],
-["Aveyron", "12", "AV","Midi-Pyrénées","regularDepartment"],
-["Bouches-du-Rhône", "13", "BR","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Calvados", "14", "CV","Basse-Normandie","regularDepartment"],
-["Cantal", "15", "CT","Auvergne","regularDepartment"],
-["Charente", "16", "CH","Poitou-Charentes","regularDepartment"],
-["Charente-Maritime", "17", "CM","Poitou-Charentes","regularDepartment"],
-["Cher", "18", "CR","Centre","regularDepartment"],
-["Corrèze", "19", "CZ","Limousin","regularDepartment"],
-["Corse-du-sud", "2A", "CS","Corse","regularDepartment"],
-["Haute-Corse", "2B", "HC","Corse","regularDepartment"],
-["Côte-d’Or", "21", "CO","Bourgogne","regularDepartment"],
-["Côtes-d’Armor", "22", "CA","Bretagne","regularDepartment"],
-["Creuse", "23", "CE","Limousin","regularDepartment"],
-["Dordogne", "24", "DG","Aquitaine","regularDepartment"],
-["Doubs", "25", "DB","Franche-Comté","regularDepartment"],
-["Drôme", "26", "DM","Rhône-Alpes","regularDepartment"],
-["Eure", "27", "ER","Haute-Normandie","regularDepartment"],
-["Eure-et-Loir", "28", "EL","Centre","regularDepartment"],
-["Finistère", "29", "FI","Bretagne","regularDepartment"],
-["Gard", "30", "GA","Languedoc-Roussillon","regularDepartment"],
-["Haute-Garonne", "31", "HG","Midi-Pyrénées","regularDepartment"],
-["Gers", "32", "GR","Midi-Pyrénées","regularDepartment"],
-["Gironde", "33", "GD","Aquitaine","regularDepartment"],
-["Hérault", "34", "HT","Languedoc-Roussillon","regularDepartment"],
-["Ille-et-Vilaine", "35", "IV","Bretagne","regularDepartment"],
-["Indre", "36", "ID","Centre","regularDepartment"],
-["Indre-et-Loire", "37", "IL","Centre","regularDepartment"],
-["Isère", "38", "IS","Rhône-Alpes","regularDepartment"],
-["Jura", "39", "JR","Franche-Comté","regularDepartment"],
-["Landes", "40", "LD","Aquitaine","regularDepartment"],
-["Loir-et-Cher", "41", "LC","Centre","regularDepartment"],
-["Loire", "42", "LR","Rhône-Alpes","regularDepartment"],
-["Haute-Loire", "43", "HL","Auvergne","regularDepartment"],
-["Loire-Atlantique", "44", "LA","Pays de la Loire","regularDepartment"],
-["Loiret", "45", "LT","Centre","regularDepartment"],
-["Lot", "46", "LO","Midi-Pyrénées","regularDepartment"],
-["Lot-et-Garonne", "47", "LG","Aquitaine","regularDepartment"],
-["Lozère", "48", "LZ","Languedoc-Roussillon","regularDepartment"],
-["Maine-et-Loire", "49", "ML","Pays de la Loire","regularDepartment"],
-["Manche", "50", "MN","Basse-Normandie","regularDepartment"],
-["Marne", "51", "MR","Champagne-Ardenne","regularDepartment"],
-["Haute-Marne", "52", "HM","Champagne-Ardenne","regularDepartment"],
-["Mayenne", "53", "MY","Pays de la Loire","regularDepartment"],
-["Meurthe-et-Moselle", "54", "MM","Lorraine","regularDepartment"],
-["Meuse", "55", "MS","Lorraine","regularDepartment"],
-["Morbihan", "56", "MH","Bretagne","regularDepartment"],
-["Moselle", "57", "MO","Lorraine","regularDepartment"],
-["Nièvre", "58", "NV","Bourgogne","regularDepartment"],
-["Nord", "59", "ND","Nord-Pas-de-Calais","regularDepartment"],
-["Oise", "60", "OI","Picardie","regularDepartment"],
-["Orne", "61", "OR","Basse-Normandie","regularDepartment"],
-["Pas-de-Calais", "62", "PC","Nord-Pas-de-Calais","regularDepartment"],
-["Puy-de-Dôme", "63", "PD","Auvergne","regularDepartment"],
-["Pyrénées-Atlantiques", "64", "PA","Aquitaine","regularDepartment"],
-["Hautes-Pyrénées", "65", "HP","Midi-Pyrénées","regularDepartment"],
-["Pyrénées-Orientales", "66", "PO","Languedoc-Roussillon","regularDepartment"],
-["Bas-Rhin", "67", "BH","Alsace","regularDepartment"],
-["Haut-Rhin", "68", "HR","Alsace","regularDepartment"],
-["Rhône", "69", "RÔ","Rhône-Alpes","regularDepartment"],
-["Haute-Saône", "70", "HS","Franche-Comté","regularDepartment"],
-["Saône-et-Loire", "71", "SL","Bourgogne","regularDepartment"],
-["Sarthe", "72", "ST","Pays de la Loire","regularDepartment"],
-["Savoie", "73", "SV","Rhône-Alpes","regularDepartment"],
-["Haute-Savoie", "74", "HO","Rhône-Alpes","regularDepartment"],
-["Paris", "75", "PR","Île-de-France","regularDepartment"],
-["Seine-Maritime", "76", "SR","Haute-Normandie","regularDepartment"],
-["Seine-et-Marne", "77", "SM","Île-de-France","regularDepartment"],
-["Yvelines", "78", "YV","Île-de-France","regularDepartment"],
-["Deux-Sèvres", "79", "DS","Poitou-Charentes","regularDepartment"],
-["Somme", "80", "SO","Picardie","regularDepartment"],
-["Tarn", "81", "TR","Midi-Pyrénées","regularDepartment"],
-["Tarn-et-Garonne", "82", "TG","Midi-Pyrénées","regularDepartment"],
-["Var", "83", "VR","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Vaucluse", "84", "VA","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Vendée", "85", "VD","Pays de la Loire","regularDepartment"],
-["Vienne", "86", "VI","Poitou-Charentes","regularDepartment"],
-["Haute-Vienne", "87", "HV","Limousin","regularDepartment"],
-["Vosges", "88", "VG","Lorraine","regularDepartment"],
-["Yonne", "89", "YN","Bourgogne","regularDepartment"],
-["Territoire de Belfort", "90", "TB","Franche-Comté","regularDepartment"],
-["Essonne", "91", "ES","Île-de-France","regularDepartment"],
-["Hauts-de-Seine", "92", "HI","Île-de-France","regularDepartment"],
-["Seine-Saint-Denis", "93", "SD","Île-de-France","regularDepartment"],
-["Val-de-Marne", "94", "VM","Île-de-France","regularDepartment"],
-["Val-d’Oise", "95", "VO","Île-de-France","regularDepartment"]
-]
+progname = "frdepartment"
 
 usage = "usage: %prog [ [-n] [-a] [-l] [-r] ( NAME | NUMERICAL_CODE | ALPHBETIC_CODE ) | --list | -v | -vv ]"
 parser = OptionParser(usage=usage, version="%prog 0.1")
@@ -115,9 +17,10 @@ parser.add_option("-l",  "--litteral",   help="print the full litteral name of t
 parser.add_option("-r",  "--region",     help="print the full-name of the region witch the department is bellonging to", default=False, action="store_true")
 parser.add_option("--list",              help="show a table of the departments with theres codes and exit",              default=False, action="store_true", dest="verbose")
 parser.add_option("-v",  "--verbose",    help="print the whole information on the output (it's like -nalr)",             default=False, action="store_true", dest="verbose")
-parser.add_option("--vv",                help="print the whole information in a litteral sentence",                      default=False, action="store_true", dest="verbose")
+parser.add_option("--vv",                help="print the whole information in a litteral sentence",                     default=False, action="store_true", dest="verbose")
 
 (options, args) = parser.parse_args()
+<<<<<<< HEAD
 
 
 def askedDepartment(nuberOrCodeToEvaluate):
@@ -128,3 +31,5 @@ def askedDepartment(nuberOrCodeToEvaluate):
 
 if sys.argv[1] != ['-h', '-v', '--vv', '--list']:
 	print askedDepartment(sys.argv[2])
+=======
+>>>>>>> parent of 76a4a61... The correspondance table was added and now, the script is responding
