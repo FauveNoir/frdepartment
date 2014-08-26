af6d01f76738c8f99777aa92e8e35acb50d6d434
Fauve <contact@taniere.info>
2014-08-26 19:14:36 +0100

    Fix the case of overseas department, territories and dependences

--- frdepartment.py
+++ frdepartment.py
@@ -7,102 +7,118 @@ from optparse import OptionParser
 import argparse
 
 coresp = [
-["Ain", "01", "AN","Rhône-Alpes","regularDepartment"],
-["Aisne", "02", "AS","Picardie","regularDepartment"],
-["Allier", "03", "AL","Auvergne","regularDepartment"],
-["Alpes-de-Haute-Provence", "04", "AH","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Hautes-Alpes", "05", "HA","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Alpes-Maritimes", "06", "AM","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Ardèche", "07", "AC","Rhône-Alpes","regularDepartment"],
-["Ardennes", "08", "AR","Champagne-Ardenne","regularDepartment"],
-["Ariège", "09", "AG","Midi-Pyrénées","regularDepartment"],
-["Aube", "10", "AB","Champagne-Ardenne","regularDepartment"],
-["Aude", "11", "AD","Languedoc-Roussillon","regularDepartment"],
-["Aveyron", "12", "AV","Midi-Pyrénées","regularDepartment"],
-["Bouches-du-Rhône", "13", "BR","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Calvados", "14", "CV","Basse-Normandie","regularDepartment"],
-["Cantal", "15", "CT","Auvergne","regularDepartment"],
-["Charente", "16", "CH","Poitou-Charentes","regularDepartment"],
-["Charente-Maritime", "17", "CM","Poitou-Charentes","regularDepartment"],
-["Cher", "18", "CR","Centre","regularDepartment"],
-["Corrèze", "19", "CZ","Limousin","regularDepartment"],
-["Corse-du-sud", "2A", "CS","Corse","regularDepartment"],
-["Haute-Corse", "2B", "HC","Corse","regularDepartment"],
-["Côte-d’Or", "21", "CO","Bourgogne","regularDepartment"],
-["Côtes-d’Armor", "22", "CA","Bretagne","regularDepartment"],
-["Creuse", "23", "CE","Limousin","regularDepartment"],
-["Dordogne", "24", "DG","Aquitaine","regularDepartment"],
-["Doubs", "25", "DB","Franche-Comté","regularDepartment"],
-["Drôme", "26", "DM","Rhône-Alpes","regularDepartment"],
-["Eure", "27", "ER","Haute-Normandie","regularDepartment"],
-["Eure-et-Loir", "28", "EL","Centre","regularDepartment"],
-["Finistère", "29", "FI","Bretagne","regularDepartment"],
-["Gard", "30", "GA","Languedoc-Roussillon","regularDepartment"],
-["Haute-Garonne", "31", "HG","Midi-Pyrénées","regularDepartment"],
-["Gers", "32", "GR","Midi-Pyrénées","regularDepartment"],
-["Gironde", "33", "GD","Aquitaine","regularDepartment"],
-["Hérault", "34", "HT","Languedoc-Roussillon","regularDepartment"],
-["Ille-et-Vilaine", "35", "IV","Bretagne","regularDepartment"],
-["Indre", "36", "ID","Centre","regularDepartment"],
-["Indre-et-Loire", "37", "IL","Centre","regularDepartment"],
-["Isère", "38", "IS","Rhône-Alpes","regularDepartment"],
-["Jura", "39", "JR","Franche-Comté","regularDepartment"],
-["Landes", "40", "LD","Aquitaine","regularDepartment"],
-["Loir-et-Cher", "41", "LC","Centre","regularDepartment"],
-["Loire", "42", "LR","Rhône-Alpes","regularDepartment"],
-["Haute-Loire", "43", "HL","Auvergne","regularDepartment"],
-["Loire-Atlantique", "44", "LA","Pays de la Loire","regularDepartment"],
-["Loiret", "45", "LT","Centre","regularDepartment"],
-["Lot", "46", "LO","Midi-Pyrénées","regularDepartment"],
-["Lot-et-Garonne", "47", "LG","Aquitaine","regularDepartment"],
-["Lozère", "48", "LZ","Languedoc-Roussillon","regularDepartment"],
-["Maine-et-Loire", "49", "ML","Pays de la Loire","regularDepartment"],
-["Manche", "50", "MN","Basse-Normandie","regularDepartment"],
-["Marne", "51", "MR","Champagne-Ardenne","regularDepartment"],
-["Haute-Marne", "52", "HM","Champagne-Ardenne","regularDepartment"],
-["Mayenne", "53", "MY","Pays de la Loire","regularDepartment"],
-["Meurthe-et-Moselle", "54", "MM","Lorraine","regularDepartment"],
-["Meuse", "55", "MS","Lorraine","regularDepartment"],
-["Morbihan", "56", "MH","Bretagne","regularDepartment"],
-["Moselle", "57", "MO","Lorraine","regularDepartment"],
-["Nièvre", "58", "NV","Bourgogne","regularDepartment"],
-["Nord", "59", "ND","Nord-Pas-de-Calais","regularDepartment"],
-["Oise", "60", "OI","Picardie","regularDepartment"],
-["Orne", "61", "OR","Basse-Normandie","regularDepartment"],
-["Pas-de-Calais", "62", "PC","Nord-Pas-de-Calais","regularDepartment"],
-["Puy-de-Dôme", "63", "PD","Auvergne","regularDepartment"],
-["Pyrénées-Atlantiques", "64", "PA","Aquitaine","regularDepartment"],
-["Hautes-Pyrénées", "65", "HP","Midi-Pyrénées","regularDepartment"],
-["Pyrénées-Orientales", "66", "PO","Languedoc-Roussillon","regularDepartment"],
-["Bas-Rhin", "67", "BH","Alsace","regularDepartment"],
-["Haut-Rhin", "68", "HR","Alsace","regularDepartment"],
-["Rhône", "69", "RÔ","Rhône-Alpes","regularDepartment"],
-["Haute-Saône", "70", "HS","Franche-Comté","regularDepartment"],
-["Saône-et-Loire", "71", "SL","Bourgogne","regularDepartment"],
-["Sarthe", "72", "ST","Pays de la Loire","regularDepartment"],
-["Savoie", "73", "SV","Rhône-Alpes","regularDepartment"],
-["Haute-Savoie", "74", "HO","Rhône-Alpes","regularDepartment"],
-["Paris", "75", "PR","Île-de-France","regularDepartment"],
-["Seine-Maritime", "76", "SR","Haute-Normandie","regularDepartment"],
-["Seine-et-Marne", "77", "SM","Île-de-France","regularDepartment"],
-["Yvelines", "78", "YV","Île-de-France","regularDepartment"],
-["Deux-Sèvres", "79", "DS","Poitou-Charentes","regularDepartment"],
-["Somme", "80", "SO","Picardie","regularDepartment"],
-["Tarn", "81", "TR","Midi-Pyrénées","regularDepartment"],
-["Tarn-et-Garonne", "82", "TG","Midi-Pyrénées","regularDepartment"],
-["Var", "83", "VR","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Vaucluse", "84", "VA","Provence-Alpes-Côte d’Azur","regularDepartment"],
-["Vendée", "85", "VD","Pays de la Loire","regularDepartment"],
-["Vienne", "86", "VI","Poitou-Charentes","regularDepartment"],
-["Haute-Vienne", "87", "HV","Limousin","regularDepartment"],
-["Vosges", "88", "VG","Lorraine","regularDepartment"],
-["Yonne", "89", "YN","Bourgogne","regularDepartment"],
-["Territoire de Belfort", "90", "TB","Franche-Comté","regularDepartment"],
-["Essonne", "91", "ES","Île-de-France","regularDepartment"],
-["Hauts-de-Seine", "92", "HI","Île-de-France","regularDepartment"],
-["Seine-Saint-Denis", "93", "SD","Île-de-France","regularDepartment"],
-["Val-de-Marne", "94", "VM","Île-de-France","regularDepartment"],
-["Val-d’Oise", "95", "VO","Île-de-France","regularDepartment"]
+["Ain", "01", "AN","Rhône-Alpes","metropolitan-department"],
+["Aisne", "02", "AS","Picardie","metropolitan-department"],
+["Allier", "03", "AL","Auvergne","metropolitan-department"],
+["Alpes-de-Haute-Provence", "04", "AH","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Hautes-Alpes", "05", "HA","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Alpes-Maritimes", "06", "AM","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Ardèche", "07", "AC","Rhône-Alpes","metropolitan-department"],
+["Ardennes", "08", "AR","Champagne-Ardenne","metropolitan-department"],
+["Ariège", "09", "AG","Midi-Pyrénées","metropolitan-department"],
+["Aube", "10", "AB","Champagne-Ardenne","metropolitan-department"],
+["Aude", "11", "AD","Languedoc-Roussillon","metropolitan-department"],
+["Aveyron", "12", "AV","Midi-Pyrénées","metropolitan-department"],
+["Bouches-du-Rhône", "13", "BR","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Calvados", "14", "CV","Basse-Normandie","metropolitan-department"],
+["Cantal", "15", "CT","Auvergne","metropolitan-department"],
+["Charente", "16", "CH","Poitou-Charentes","metropolitan-department"],
+["Charente-Maritime", "17", "CM","Poitou-Charentes","metropolitan-department"],
+["Cher", "18", "CR","Centre","metropolitan-department"],
+["Corrèze", "19", "CZ","Limousin","metropolitan-department"],
+["Corse-du-sud", "2A", "CS","Corse","metropolitan-department"],
+["Haute-Corse", "2B", "HC","Corse","metropolitan-department"],
+["Côte-d’Or", "21", "CO","Bourgogne","metropolitan-department"],
+["Côtes-d’Armor", "22", "CA","Bretagne","metropolitan-department"],
+["Creuse", "23", "CE","Limousin","metropolitan-department"],
+["Dordogne", "24", "DG","Aquitaine","metropolitan-department"],
+["Doubs", "25", "DB","Franche-Comté","metropolitan-department"],
+["Drôme", "26", "DM","Rhône-Alpes","metropolitan-department"],
+["Eure", "27", "ER","Haute-Normandie","metropolitan-department"],
+["Eure-et-Loir", "28", "EL","Centre","metropolitan-department"],
+["Finistère", "29", "FI","Bretagne","metropolitan-department"],
+["Gard", "30", "GA","Languedoc-Roussillon","metropolitan-department"],
+["Haute-Garonne", "31", "HG","Midi-Pyrénées","metropolitan-department"],
+["Gers", "32", "GR","Midi-Pyrénées","metropolitan-department"],
+["Gironde", "33", "GD","Aquitaine","metropolitan-department"],
+["Hérault", "34", "HT","Languedoc-Roussillon","metropolitan-department"],
+["Ille-et-Vilaine", "35", "IV","Bretagne","metropolitan-department"],
+["Indre", "36", "ID","Centre","metropolitan-department"],
+["Indre-et-Loire", "37", "IL","Centre","metropolitan-department"],
+["Isère", "38", "IS","Rhône-Alpes","metropolitan-department"],
+["Jura", "39", "JR","Franche-Comté","metropolitan-department"],
+["Landes", "40", "LD","Aquitaine","metropolitan-department"],
+["Loir-et-Cher", "41", "LC","Centre","metropolitan-department"],
+["Loire", "42", "LR","Rhône-Alpes","metropolitan-department"],
+["Haute-Loire", "43", "HL","Auvergne","metropolitan-department"],
+["Loire-Atlantique", "44", "LA","Pays de la Loire","metropolitan-department"],
+["Loiret", "45", "LT","Centre","metropolitan-department"],
+["Lot", "46", "LO","Midi-Pyrénées","metropolitan-department"],
+["Lot-et-Garonne", "47", "LG","Aquitaine","metropolitan-department"],
+["Lozère", "48", "LZ","Languedoc-Roussillon","metropolitan-department"],
+["Maine-et-Loire", "49", "ML","Pays de la Loire","metropolitan-department"],
+["Manche", "50", "MN","Basse-Normandie","metropolitan-department"],
+["Marne", "51", "MR","Champagne-Ardenne","metropolitan-department"],
+["Haute-Marne", "52", "HM","Champagne-Ardenne","metropolitan-department"],
+["Mayenne", "53", "MY","Pays de la Loire","metropolitan-department"],
+["Meurthe-et-Moselle", "54", "MM","Lorraine","metropolitan-department"],
+["Meuse", "55", "MS","Lorraine","metropolitan-department"],
+["Morbihan", "56", "MH","Bretagne","metropolitan-department"],
+["Moselle", "57", "MO","Lorraine","metropolitan-department"],
+["Nièvre", "58", "NV","Bourgogne","metropolitan-department"],
+["Nord", "59", "ND","Nord-Pas-de-Calais","metropolitan-department"],
+["Oise", "60", "OI","Picardie","metropolitan-department"],
+["Orne", "61", "OR","Basse-Normandie","metropolitan-department"],
+["Pas-de-Calais", "62", "PC","Nord-Pas-de-Calais","metropolitan-department"],
+["Puy-de-Dôme", "63", "PD","Auvergne","metropolitan-department"],
+["Pyrénées-Atlantiques", "64", "PA","Aquitaine","metropolitan-department"],
+["Hautes-Pyrénées", "65", "HP","Midi-Pyrénées","metropolitan-department"],
+["Pyrénées-Orientales", "66", "PO","Languedoc-Roussillon","metropolitan-department"],
+["Bas-Rhin", "67", "BH","Alsace","metropolitan-department"],
+["Haut-Rhin", "68", "HR","Alsace","metropolitan-department"],
+["Rhône", "69", "RÔ","Rhône-Alpes","metropolitan-department"],
+["Haute-Saône", "70", "HS","Franche-Comté","metropolitan-department"],
+["Saône-et-Loire", "71", "SL","Bourgogne","metropolitan-department"],
+["Sarthe", "72", "ST","Pays de la Loire","metropolitan-department"],
+["Savoie", "73", "SV","Rhône-Alpes","metropolitan-department"],
+["Haute-Savoie", "74", "HO","Rhône-Alpes","metropolitan-department"],
+["Paris", "75", "PR","Île-de-France","metropolitan-department"],
+["Seine-Maritime", "76", "SR","Haute-Normandie","metropolitan-department"],
+["Seine-et-Marne", "77", "SM","Île-de-France","metropolitan-department"],
+["Yvelines", "78", "YV","Île-de-France","metropolitan-department"],
+["Deux-Sèvres", "79", "DS","Poitou-Charentes","metropolitan-department"],
+["Somme", "80", "SO","Picardie","metropolitan-department"],
+["Tarn", "81", "TR","Midi-Pyrénées","metropolitan-department"],
+["Tarn-et-Garonne", "82", "TG","Midi-Pyrénées","metropolitan-department"],
+["Var", "83", "VR","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Vaucluse", "84", "VA","Provence-Alpes-Côte d’Azur","metropolitan-department"],
+["Vendée", "85", "VD","Pays de la Loire","metropolitan-department"],
+["Vienne", "86", "VI","Poitou-Charentes","metropolitan-department"],
+["Haute-Vienne", "87", "HV","Limousin","metropolitan-department"],
+["Vosges", "88", "VG","Lorraine","metropolitan-department"],
+["Yonne", "89", "YN","Bourgogne","metropolitan-department"],
+["Territoire de Belfort", "90", "TB","Franche-Comté","metropolitan-department"],
+["Essonne", "91", "ES","Île-de-France","metropolitan-department"],
+["Hauts-de-Seine", "92", "HI","Île-de-France","metropolitan-department"],
+["Seine-Saint-Denis", "93", "SD","Île-de-France","metropolitan-department"],
+["Val-de-Marne", "94", "VM","Île-de-France","metropolitan-department"],
+["Val-d’Oise", "95", "VO","Île-de-France","metropolitan-department"],
+
+["Saint-Barthélemy", "977", "BL", None, "overseas-collectivity", "ISO 3166-2:BL"],
+["Saint-Martin", "978", "MF", None, "overseas-collectivity", "ISO 3166-2:MF"],
+["Nouvelle-Calédonie", "988", "NC", None, "overseas-collectivity", "ISO 3166-2:NC"],
+["Polynésie française", "987", "PF", None, "overseas-collectivity", "ISO 3166-2:PF"],
+["Saint-Pierre-et-Miquelon", "975", "PM", None, "overseas-collectivity", "ISO 3166-2:PM"],
+["Terres australes françaises", "984", "TF", None, "overseas-collectivity", "ISO 3166-2:TF"],
+["Wallis-et-Futuna", "986", "WF", None, "overseas-collectivity", "ISO 3166-2:WF"],
+
+["Île de Clipperton", "989", "CP", None, "dependency", "ISO 3166-2:WF"],
+
+["Guyane française", "973", "GF", None, "overseas-departments", "ISO 3166-2:GF"],
+["Guadeloupe", "971", "GP", None, "overseas-departments", "ISO 3166-2:GP"],
+["Martinique", "972", "MQ", None, "overseas-departments", "ISO 3166-2:MQ"],
+["Réunion", "974", "RE", None, "overseas-departments", "ISO 3166-2:RE"],
+["Mayotte", "976", "YT", None, "overseas-departments", "ISO 3166-2:YT"]
 ]
 
 usage = "usage: %prog [ [-n] [-a] [-l] [-r] ( NAME | NUMERICAL_CODE | ALPHBETIC_CODE ) | --list | -v | -vv ]"
@@ -141,19 +157,44 @@ if options.listing == False: # listing is for printing a complete table of corre
 	findedDepratment = askedDepartment(sys.argv[2]) # evaluating witch departement the user is searching and import on findedDepartment the whol information about it to be treated.
 
 	if findedDepratment:
-		if options.vverbose:
-			if findedDepratment[0][0] in ("A", "U", "E", "I", "Y", "O"): #This test is for french syntax. Because in french languages, words begining by a voyel have to be preceded with “l’” and words  begining with a console, wave to be preced by noting.
-				linguisticConjonction = "l’"
-			else: # Other cases is a console.
-				linguisticConjonction = ""
+		if findedDepratment[0][0] in ("A", "U", "E", "I", "Y", "Î", "O"): #This test is for french syntax. Because in french languages, words begining by a voyel have to be preceded with “l’” and words  begining with a console, wave to be preced by noting.
+			linguisticConjonction = "l’"
+		else: # Other cases is a console.
+			linguisticConjonction = ""
 
-			print("Departement de {4}{0} en {1} de codes {2} (Réformé) et {3} (Insee).".format(findedDepratment[0], findedDepratment[3], findedDepratment[2], findedDepratment[1], linguisticConjonction))
+		if options.vverbose:
+
+			if findedDepratment[4] == "metropolitan-department":
+				print("Departement de {4}{0} en {1} de codes {2} (Réformé) et {3} (Insee).".format(findedDepratment[0], findedDepratment[3], findedDepratment[2], findedDepratment[1], linguisticConjonction))
+
+			if findedDepratment[4] == "overseas-departments":
+				print("Région et département d’Outre-mer de {4}{0} de codes {2} (ISO 3166-2:FR) et {3} (Insee).".format(findedDepratment[0], findedDepratment[3], findedDepratment[2], findedDepratment[1], linguisticConjonction))
+				if findedDepratment[5]:
+					print "voir aussi", findedDepratment[5] + "."
+
+			if findedDepratment[4] == "dependency":
+				print("Dépendance de {4}{0} de codes {2} (ISO 3166-2:FR) et {3} (Insee).".format(findedDepratment[0], findedDepratment[3], findedDepratment[2], findedDepratment[1], linguisticConjonction))
+				if findedDepratment[5]:
+					print "voir aussi", findedDepratment[5] + "."
 
 		else:
+			if options.reg and findedDepratment[4] != "metropolitan-department": # For overseas departement, terretiories and dependencies witch not bellong to any region.
+				print("* Le térritoire de {0}{1} est".format(linguisticConjonction, findedDepratment[0]) ),
+
+				if findedDepratment[4] == "overseas-departments":
+					print "une région mono-départementale d’outre mer."
+
+				if findedDepratment[4] == "overseas-collectivity":
+					print "un département d’outre-mer non-régionnalisé."
+
+				if findedDepratment[4] == "dependency":
+					print "une dépendance non-régionnalisé."
+
 			if options.lit:
 				print findedDepratment[0],
-			if options.reg:
+			if options.reg and findedDepratment[4] == "metropolitan-department":
 				print findedDepratment[3],
+
 			if options.alpha:
 				print findedDepratment[2],
 			if options.number:
